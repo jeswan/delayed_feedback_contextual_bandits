6243c3fce07930ddcc55a0b65f121cbc00107b38
Jesse Swanson <js11133@nyu.edu>
2021-05-15 21:38:57 -0400

    final report commit

--- src/models/ext_cb_wheel.py
+++ src/models/ext_cb_wheel.py
@@ -18,7 +18,7 @@ from tqdm import tqdm
 from contextualbandits.linreg import LinearRegression
 from copy import deepcopy
 from pylab import rcParams
-from reward_generators.ext_simple_rewards import SimpleRewardsGenerator
+from reward_generators.wheel_bandit import WheelBandit
 from sklearn.datasets import load_svmlight_file
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
@@ -28,13 +28,8 @@ from sklearn.preprocessing import MultiLabelBinarizer
 
 
 def simulate_rounds_stoch(model, rewards, actions_hist, X_batch, y_batch,
-                          items_changed, rnd_seed):
+                          rnd_seed):
     np.random.seed(rnd_seed)
-
-    for item_changed in items_changed:
-        model.drop_arm(item_changed)
-        model.add_arm(item_changed)
-        model.choice_names = np.arange(10)
 
     # choosing actions for this batch
     actions_this_batch = model.predict(X_batch).astype('uint8')
@@ -58,14 +53,14 @@ def simulate_rounds_stoch(model, rewards, actions_hist, X_batch, y_batch,
 def get_batch(batch_size, nchoices, reward_gen):
     rewards = np.zeros((batch_size, nchoices))
     reward_probs = np.zeros((batch_size, nchoices))
-    items_changed = []
+    contexts = []
     for i in range(batch_size):
-        reward_vector, item_changed = reward_gen.get_rewards()
+        context = reward_gen.generate_context()
+        reward_vector, reward_means = reward_gen.get_rewards(context)
         rewards[i] = reward_vector
-        reward_probs[i] = reward_gen.reward_probs
-        if item_changed:
-            items_changed.append(item_changed)
-    return rewards, reward_probs, items_changed
+        reward_probs[i] = reward_means
+        contexts.append(context)
+    return rewards, reward_probs, contexts
 
 
 def get_mean_reward(reward_lst, batch_size):
@@ -75,7 +70,8 @@ def get_mean_reward(reward_lst, batch_size):
     return mean_rew
 
 
-def worker(worker_num, model, batch_size, nchoices, n_steps):
+def worker(worker_num, model, batch_size, nchoices, n_steps,
+           exploration_param=0.5):
     # batch size - algorithms will be refit after N rounds
 
     # These lists will keep track of the rewards obtained by each policy
@@ -85,15 +81,15 @@ def worker(worker_num, model, batch_size, nchoices, n_steps):
     lst_rewards = []
 
     # initial seed - all policies start with the same small random selection of actions/rewards
-    reward_gen = SimpleRewardsGenerator(change_prob=1e-3)
+    reward_gen = WheelBandit(delta=exploration_param)
 
     init_reward_vector = np.zeros((batch_size, nchoices))
     for i in range(batch_size):
-        reward_vector, _ = reward_gen.get_rewards()
+        reward_vector, _ = reward_gen.get_rewards(reward_gen.generate_context())
         init_reward_vector[i] = reward_vector
 
     np.random.seed(1)
-    first_batch = np.zeros((batch_size, 1))
+    first_batch = np.zeros((batch_size, 2))  # dim of contexts
     action_chosen = np.random.randint(nchoices, size=batch_size)
     rewards_received = init_reward_vector[np.arange(batch_size), action_chosen]
 
@@ -103,34 +99,34 @@ def worker(worker_num, model, batch_size, nchoices, n_steps):
     # these lists will keep track of which actions does each policy choose
     lst_actions = [action_chosen.copy()]
 
-    cumul_regrets = []
+    ret_regrets = []
     ret_rewards = []
 
     # now running all the simulation
     for i in range(int(np.floor(n_steps / batch_size))):
         batch_st = (i + 1) * batch_size
-        X_batch = np.zeros((batch_size, 1))
-        rewards, reward_probs, items_changed = get_batch(batch_size, nchoices,
-                                                         reward_gen)
+        rewards, reward_probs, contexts = get_batch(batch_size,
+                                                    nchoices,
+                                                    reward_gen)
+        X_batch = np.array(contexts)
         y_batch = rewards
 
         actions_this_batch, lst_actions, rewards_batch = simulate_rounds_stoch(model,
                                                                                lst_rewards,
                                                                                lst_actions,
                                                                                X_batch, y_batch,
-                                                                               items_changed,
                                                                                rnd_seed=batch_st)
 
-        cumul_regrets.extend(
+        ret_regrets.extend(
             np.max(reward_probs, axis=1) -
             reward_probs[np.arange(batch_size), actions_this_batch]
         )
         ret_rewards.extend(rewards_batch)
 
-    return cumul_regrets, ret_rewards
+    return ret_regrets, ret_rewards
 
 
-def beta_prior_calc(beta_priors, nchoices=10, is_ts=False):
+def beta_prior_calc(beta_priors, nchoices=5, is_ts=False):
     '''
     nchoices is the number of arms
     returns list of tuples, each of which can be tried as hps
@@ -143,7 +139,7 @@ def beta_prior_calc(beta_priors, nchoices=10, is_ts=False):
 if __name__ == "__main__":
     NUM_TRIALS = 10
     DELTAS = [1, 10, 100, 1000]
-    NUM_ITEMS = 10
+    NUM_ITEMS = 5
     n_steps = 10000
 
     nchoices = NUM_ITEMS
@@ -152,12 +148,12 @@ if __name__ == "__main__":
     base_ols = LinearRegression(lambda_=10., fit_intercept=True, method="sm")
 
     beta_priors_ag = [7.0]
-    beta_priors_eg = [1]
-    beta_priors_ucb = [7]
-    beta_priors_ts = [3]
-    beta_priors_ae = [3]
+    beta_priors_eg = [3]
+    beta_priors_ucb = [1]
+    beta_priors_ts = [1]
+    beta_priors_ae = [1]
 
-    print(f"beta_priors: {beta_prior_calc(beta_priors_eg)}")
+    # print(f"beta_priors: {beta_prior_calc(beta_priors_ag)}")
     # print(‘UCB_priors: ’, beta_prior_calc(beta_prior_ucb))
     # print(‘TS_prios: ’, beta_prior_calc(beta_prior_ts, is_ts=True))
 
@@ -198,12 +194,9 @@ if __name__ == "__main__":
                                    range(NUM_TRIALS))
                 mean_regret_results[i][j] = np.array([x[0] for x in
                                                       results]).sum(axis=0) / NUM_TRIALS
+
                 mean_reward_results[i][j] = np.array([x[1] for x in
                                                       results]).sum(axis=0) / NUM_TRIALS
-    # print(f"regret results: {mean_regret_results}")
-    # print(f"regret sum: {np.sum(mean_regret_results, axis=1)}")
-    # print(f"rewards cumsum: {np.mean(mean_reward_results[0][0].reshape(-1, int(n_steps/100)), axis=1)}")
-    # print(f"reward resultsl {mean_reward_results}")
 
     fig, axs = plt.subplots(2, 2)
 
@@ -226,13 +219,10 @@ if __name__ == "__main__":
 
         ax.set(xlabel='Rounds', ylabel='Cumulative Mean Reward', title=f"$\delta$ = {DELTAS[i]}")
 
-        ax.set_ylim([0.3, 1])
+        ax.set_ylim([0, 20])
 
         if i == 0:
-            ax.legend(loc="upper right", prop={'size': 7})
+            ax.legend(loc="lower right", prop={'size': 7})
 
-    for ax in axs.flat:
-        ax.label_outer()
-
-    fig.suptitle('Comparison of Online Bandit Policies with Delayed Feedback with No Context Simulator')
+    fig.suptitle('Comparison of Online Contextual Bandit Policies with Delayed with Wheel Simulator')
     plt.show()
